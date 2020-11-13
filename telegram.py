bf163ad0743ab26e9337fbab8fcbf6b07bb9240f
garmidan <garmidan16@gmail.com>
2020-11-13 11:38:41 -0500

    Completado los dos ejemplos

--- telegram.py
+++ telegram.py
@@ -15,7 +15,7 @@ import requests
 def telegram_bot_sendtext(bot_message):
    bot_token = '1444469431:AAHGiijQJ952yuD8MA2TBLQKQs4uovypng4'
    bot_chatID = '-411732112'
-   bot_message = 'Mensaje enviado desde python'
+   bot_message = 'Mensaje enviado desde python 2'
    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
    response = requests.get(send_text)
--- telegram2.py
+++ telegram2.py
@@ -1,43 +1,45 @@
 # importar todas las bibliotecas requeridas
 import telebot 
 from telethon.sync import TelegramClient 
-from telethon.tl.types import InputPeerUser, InputPeerChannel 
+from telethon.tl.types import InputPeerUser, InputPeerChannel, InputChannelEmpty
 from telethon import TelegramClient, sync, events 
+from telethon.tl.functions.contacts import ResolveUsernameRequest
+from telethon.tl.types.messages import Messages
+from telethon.tl.types.contacts import Contacts
+from telethon.tl.functions.contacts import GetContactsRequest
+import requests
   
    
 # obtener datos de la api (api_id, api_hash, token)
 # desde el telegrama como se describe arriba
-api_id = 'API_id'
-api_hash = 'API_hash'
-token = 'bot token'
-  
+api_id = '2874192'
+api_hash = 'e0010897cd27a19d8722b1d9450400f9'
+token = 'bot1444469431:AAHGiijQJ952yuD8MA2TBLQKQs4uovypng4'
+mensaje = 1
 # Numero de celular 
-phone = 'YOUR_PHONE_NUMBER_WTH_COUNTRY_CODE'
+phone = '+573186492691'
    
 # creando una sesión de telegrama y asignando
 # Variable cliente 
 client = TelegramClient('session', api_id, api_hash) 
-   
 # Probar conexion de telegram
-client.connect() 
-  
+client.connect()
 # en caso de que el script se ejecute por primera vez
 # pida que introduzca el token o el otp enviado a
 # número enviado o su ID de telegrama
 if not client.is_user_authorized(): 
-   
     client.send_code_request(phone) 
-      
     # firmando en el cliente
     client.sign_in(phone, input('Enter the code: ')) 
    
-   
 try: 
     # El receptor
-    receiver = InputPeerUser('user_id', 'user_hash') 
-  
-    # enviando mensaje usando el cliente de telegram 
-    client.send_message(receiver, "Message", parse_mode='html') 
+    contacts = client(GetContactsRequest(0))
+    for u in contacts.users: 
+        if u.phone == "573123189219":
+            client.send_message(InputPeerUser(u.id, u.access_hash), "Hola desde python") 
+            # enviando mensaje usando el cliente de telegram 
+            print("Mensaje Enviado a cliente telegram")
 except Exception as e: 
       
     # there may be many error coming in while like peer 
